f38b654744c01d7c9547f053917b202b9285406d
fivdi <fivdiweb@gmail.com>
2015-11-02 11:11:55 +0100

    read performance test

---
+++ test/digital-read-performance.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var Gpio = require('../'),
+  button = new Gpio(4, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_DOWN
+  }),
+  time,
+  ops,
+  i;
+
+time = process.hrtime();
+
+for (i = 0; i !== 2000000; i += 1) {
+  button.digitalRead();
+}
+
+time = process.hrtime(time);
+ops = Math.floor(i / (time[0] + time[1] / 1E9));
+
+console.log('  ' + ops + ' read ops per second');
+
