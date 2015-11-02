0b66ea21c4678509d95a23f9a1698a55add3c8f7
fivdi <fivdiweb@gmail.com>
2015-11-02 10:45:20 +0100

    improved light-switch test

--- test/light-switch.js
+++ test/light-switch.js
@@ -1,7 +1,11 @@
 'use strict';
 
 var Gpio = require('../'),
-  button = new Gpio(4, {mode: Gpio.INPUT, edge: Gpio.EITHER_EDGE}),
+  button = new Gpio(4, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_DOWN,
+    edge: Gpio.EITHER_EDGE
+  }),
   led = new Gpio(17, {mode: Gpio.OUTPUT});
 
 button.on('interrupt', function (gpio, level, tick) {
