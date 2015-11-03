ffd96a90e38b52a67e2239894f4d6bb5c56826a3
fivdi <fivdiweb@gmail.com>
2015-11-03 16:58:46 +0100

    improved multiple interrupt sources test

--- test/isr-multiple-sources.js
+++ test/isr-multiple-sources.js
@@ -4,38 +4,29 @@
 // - GPIO7 needs to be connected to GPIO8 with a 1K resistor
 // - GPIO9 needs to be connected to GPIO11 with a 1K resistor
 
-var Gpio = require('../'),
-  interruptCounts = [0, 0],
-  inputs = [
-    new Gpio(7, {mode: Gpio.INPUT, edge: Gpio.EITHER_EDGE}),
-    new Gpio(9, {mode: Gpio.INPUT, edge: Gpio.EITHER_EDGE})
-  ],
-  outputs = [
-    new Gpio(8, {mode: Gpio.OUTPUT}),
-    new Gpio(11, {mode: Gpio.OUTPUT})
-  ];
+var Gpio = require('../');
 
-// Put inputs and outputs in a known state
-outputs.forEach(function (output) {
+[[7, 8], [9, 11]].forEach(function (gpioNos) {
+  var interruptCount= 0,
+    input = new Gpio(gpioNos[0], {mode: Gpio.INPUT, edge: Gpio.EITHER_EDGE}),
+    output = new Gpio(gpioNos[1], {mode: Gpio.OUTPUT});
+
+  // Put input and output in a known state
   output.digitalWrite(0);
-});
 
-[0, 1].forEach(function (ix) {
-  inputs[ix].on('interrupt', function (level) {
-    interruptCounts[ix] += 1;
-    outputs[ix].digitalWrite(level ^ 1);
+  input.on('interrupt', function (level) {
+    interruptCount += 1;
+    output.digitalWrite(level ^ 1);
 
-    if (interruptCounts[ix] === 1000 * (ix + 1)) {
-      console.log('  ' + interruptCounts[ix] + ' interrupts detected on input' + ix);
-      inputs[ix].disableInterrupt();
+    if (interruptCount === 1000) {
+      console.log('  ' + interruptCount + ' interrupts detected on GPIO' + gpioNos[0]);
+      input.disableInterrupt();
     }
   });
-});
 
-setTimeout(function () {
-  // Trigger first interrupts
-  outputs.forEach(function (output) {
+  setTimeout(function () {
+    // Trigger first interrupt
     output.digitalWrite(1);
-  });
-}, 1);
+  }, 1);
+});
 
