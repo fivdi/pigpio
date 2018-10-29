aa2e298f3d6d8509a54c7588d82b12140554ab0e
erikma <erikma1@hotmail.com>
2018-10-28 18:31:53 -0700

    Fix pigpio init in tick.js

--- test/tick.js
+++ test/tick.js
@@ -2,6 +2,8 @@
 
 const pigpio = require('../');
 
+pigpio.initialize();
+
 // Wait a half-millisecond using a 100% CPU spin-wait.
 // Only suitable as a test, don't do this for real as you lock up
 // the Node.js main thread with this pattern.
@@ -14,3 +16,5 @@ do {
 } while (deltaUsec < 500);
 
 console.log(`Start (us) = ${startUsec}, now (us) = ${nowUsec}, diff (us) = ${deltaUsec}`);
+
+pigpio.terminate();
