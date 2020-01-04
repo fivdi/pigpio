8aa7f80a818cca40066972f16ed16fb9423fc046
Roger Schönbächler <schoero@gmx.ch>
2020-01-04 14:36:00 +0000

    test: Added waveChain test

---
+++ test/wave-chain.js
@@ -0,0 +1,67 @@
+const assert = require('assert');
+const pigpio = require('pigpio');
+const Gpio = pigpio.Gpio;
+
+pigpio.configureClock(1, pigpio.CLOCK_PCM);
+
+const iterations = 20;
+const repetitions = 3;
+const delay = 10;
+
+const outPin = 17;
+const outPut = new Gpio(outPin, {
+  mode: Gpio.OUTPUT
+});
+
+outPut.digitalWrite(0);
+outPut.waveClear();
+
+let firstWaveForm = [];
+let secondWaveForm = [];
+let result = [];
+
+for (let x = 0; x <= iterations; x++) {
+  if (x % 2 == 0) {
+    firstWaveForm.push({ gpioOn: outPin, gpioOff: 0, usDelay: delay });
+  } else {
+    firstWaveForm.push({ gpioOn: 0, gpioOff: outPin, usDelay: delay });
+  }
+}
+
+outPut.waveAddGeneric(firstWaveForm);
+let firstWaveId = outPut.waveCreate();
+
+
+for (let x = 0; x <= iterations; x++) {
+  if (x % 2 == 0) {
+    secondWaveForm.push({ gpioOn: outPin, gpioOff: 0, usDelay: delay });
+  } else {
+    secondWaveForm.push({ gpioOn: 0, gpioOff: outPin, usDelay: delay });
+  }
+}
+
+outPut.waveAddGeneric(secondWaveForm);
+let secondWaveId = outPut.waveCreate();
+
+outPut.enableAlert();
+
+outPut.on("alert", (level, tick) => {
+  result.push([level, tick]);
+  if (result.length === iterations + (iterations * repetitions)) {
+    for (let r = 0; r < result.length; r++) {
+      if (result[r + 1] !== undefined) {
+        assert.strictEqual(Math.abs(delay - pigpio.tickDiff(result[r][1], result[r + 1][1])) < 20, true, 'Waves tick mismatch');
+      }
+    }
+    outPut.disableAlert();
+  }
+});
+
+if (firstWaveId >= 0 && secondWaveId >= 0) {
+  outPut.waveChain([firstWaveId, 255, 0, secondWaveId,	255, 1, repetitions, 0]);
+}
+
+while (outPut.waveTxBusy()) {}
+
+outPut.waveDelete(firstWaveId);
+outPut.waveDelete(secondWaveId);
